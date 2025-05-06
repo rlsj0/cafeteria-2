f8d2ec5c2e19ef5d152d1c6fc1497909f784728d
rlsj0 <rodrileja@gmail.com>
2025-05-06 22:10:33 +0200

    add: create query params objects

---
+++ backend/Models/CafeQueryParameteres.cs
@@ -0,0 +1,9 @@
+namespace CafeteriaApp.Models;
+
+public class CafeQueryParameters
+{
+    public string? Variedad { get; set; }
+    public string? Tipo { get; set; }
+    public decimal? Precio { get; set; }
+}
+
---
+++ backend/Models/UsuarioQueryParameteres.cs
@@ -0,0 +1,10 @@
+namespace CafeteriaApp.Models;
+
+public class UsuarioQueryParameteres
+{
+    public string? Rol { get; set; }
+    public string? Correo { get; set; }
+    public DateTime FechaInicio { get; set; }
+    public DateTime FechaFinal { get; set; }
+}
+
