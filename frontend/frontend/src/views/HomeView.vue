f99509cf7fd3a4c47a3d3fa98f7ea2fd2f0f8687
rlsj0 <rodrileja@gmail.com>
2025-05-09 15:51:04 +0200

    add: create login component with basic funcionality

--- frontend/frontend/src/views/HomeView.vue
+++ frontend/frontend/src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import CafeGrid from '../components/CafeGrid.vue'
+import Login from '../components/Login.vue'
 </script>
 
 <template>
   <main>
     <h1>Home</h1>
+    <Login />
     <CafeGrid />
   </main>
 </template>
