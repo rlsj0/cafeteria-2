5276533642bac4b11e5e4f04d4d9c51cd23fbedb
rlsj0 <rodrileja@gmail.com>
2025-05-15 15:44:21 +0200

    add: create admin view

--- frontend/frontend/src/router/index.ts
+++ frontend/frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import UserView from '../views/UserView.vue'
+import AdminView from '../views/AdminView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/userzone',
       name: 'UserView',
       component: UserView,
+    },
+    {
+      path: '/adminzone',
+      name: 'AdminView',
+      component: AdminView,
     },
     {
       path: '/about',
---
+++ frontend/frontend/src/views/AdminView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import LogoutBtn from '../components/LogoutBtn.vue'
+import { ref, onMounted } from 'vue';
+import { useSessionStore } from '@/stores/sessionStore'
+
+const sessionStore = useSessionStore();
+const id = sessionStore.getId;
+
+</script>
+
+<template>
+
+  <router-link to="/">Home</router-link>
+  <LogoutBtn />
+  <h1>Zona Privada de Admin</h1>
+  <h3>Listado global de pedidos: </h3>
+  <!-- <ListadoPedidosUsuario v-bind:identificador="id" />-->
+
+</template>
