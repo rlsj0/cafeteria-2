3f7837030bc825407328e22a77c30f46e3cddd98
rlsj0 <rodrileja@gmail.com>
2025-05-15 15:55:08 +0200

    add: create global list of orders in adminview

--- frontend/frontend/src/views/AdminView.vue
+++ frontend/frontend/src/views/AdminView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import LogoutBtn from '../components/LogoutBtn.vue'
+import ListadoPedidosGlobal from '../components/ListadoPedidosGlobal.vue'
 import { ref, onMounted } from 'vue';
 import { useSessionStore } from '@/stores/sessionStore'
 
@@ -15,5 +16,6 @@ const id = sessionStore.getId;
   <h1>Zona Privada de Admin</h1>
   <h3>Listado global de pedidos: </h3>
   <!-- <ListadoPedidosUsuario v-bind:identificador="id" />-->
+  <ListadoPedidosGlobal />
 
 </template>
