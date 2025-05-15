30412b27cace7c35fc655f9ac8c44c4fc7980e16
rlsj0 <rodrileja@gmail.com>
2025-05-15 22:48:28 +0200

    add: create footer

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="pa-4"></div>
+  <v-footer color="primary" class="white--text pa-4">
+    <v-row justify="center">
+      <v-col class="text-center">
+        ©2025 Cafetería 2 - Todos los derechos reservados.
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
