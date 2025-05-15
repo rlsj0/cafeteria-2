6f87a928a95f601078cefcf7774eac2bc4ead597
rlsj0 <rodrileja@gmail.com>
2025-05-15 23:01:32 +0200

    fix: correct footer width

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="pa-4"></div>
-  <v-footer color="primary" class="white--text pa-4">
+  <v-footer app color="primary" class="white--text pa-4 w-100">
     <v-row justify="center">
       <v-col class="text-center">
         ©2025 Cafetería 2 - Todos los derechos reservados.
